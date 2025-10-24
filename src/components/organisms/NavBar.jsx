c7874e457178e90434b81f2b8583360dcaca1ee6
Ivan <idavidcastro@unicesar.edu.co>
2025-10-24 17:43:58 -0500

    feat: enhance NavBar component with dropdown menu for improved navigation

--- src/components/organisms/NavBar.jsx
+++ src/components/organisms/NavBar.jsx
@@ -1,10 +1,11 @@
-import { X } from "lucide-react";
 import { useEffect, useState } from "react";
 import { MdMenu } from "react-icons/md";
+import { ChevronDown, ChevronUp } from "lucide-react";
 
 export default function NavBar() {
   const [open, setOpen] = useState(false);
   const links = [
+    { name: "Inicio", href: "/" },
     { name: "Populares", href: "/allmovies?category=popular" },
     { name: "Recomendadas", href: "/allmovies?category=top-rated" },
     { name: "Próximamente", href: "/allmovies?category=upcoming" },
@@ -52,16 +53,16 @@ export default function NavBar() {
   return (
     <nav
       className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${
-        scrolled || open
+        scrolled
           ? "bg-transparent backdrop-blur-md shadow-lg"
           : "bg-transparent"
       }`}
     >
-      <div className="px-[5%] py-4 flex items-center justify-between">
+      <div className="px-[5%] py-2 lg:py-4 flex items-center justify-between">
         <a href="/" className="text-2xl font-bold text-white cursor-pointer">
           moov<span className="text-secondary">i</span>x
         </a>
-        <ul className="hidden gap-8 font-medium md:flex text-primary">
+        <ul className="hidden gap-8 font-medium lg:flex text-primary">
           {links.map((link) => (
             <li key={link.name} className="relative cursor-pointer group">
               <a
@@ -74,29 +75,42 @@ export default function NavBar() {
           ))}
         </ul>
         <button
-          className="md:hidden text-primary"
+          className={`lg:hidden  flex rounded-md px-2 py-2 gap-1 font-bold text-base items-center ${
+            open ? "bg-primary text-bg-secondary" : "text-primary "
+          }`}
           onClick={() => setOpen(!open)}
         >
-          {open ? <X size={24} /> : <MdMenu size={24} />}
+          Menú
+          {open ? (
+            <ChevronUp size={18} strokeWidth={3} />
+          ) : (
+            <ChevronDown size={18} strokeWidth={3} />
+          )}
         </button>
       </div>
-      <div
-        className={`md:hidden transition-all duration-300 items-center flex justify-center ${
-          open ? "opacity-100" : "max-h-0 opacity-0"
-        } overflow-hidden h-screen`}
-      >
-        <ul className="flex flex-col gap-6 font-medium text-primary text-center">
-          {links.map((link) => (
-            <li key={link.name}>
-              <a
-                href={link.href}
-                onClick={(e) => handleLinkClick(e, link.href)}
-              >
-                {link.name}
-              </a>
-            </li>
-          ))}
-        </ul>
+
+      <div className="relative lg:hidden">
+        {open && (
+          <div className="relative lg:hidden">
+            {open && (
+              <div className="absolute right-4 top-0 p-2 bg-bg/80 backdrop:md text-primary rounded-xl py-3 w-56 z-40 backdrop-blur-md shadow-lg">
+                <ul className="flex flex-col text-sm font-medium">
+                  {links.map((link) => (
+                    <li key={link.name}>
+                      <a
+                        href={link.href}
+                        onClick={(e) => handleLinkClick(e, link.href)}
+                        className="block px-5 py-2 hover:bg-primary hover:text-bg rounded-lg transition-colors font-semibold"
+                      >
+                        {link.name}
+                      </a>
+                    </li>
+                  ))}
+                </ul>
+              </div>
+            )}
+          </div>
+        )}
       </div>
     </nav>
   );
