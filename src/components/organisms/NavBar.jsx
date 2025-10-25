e909c3f8256d6c7032fd6a16a1bf73f0d2ccec75
Ivan <idavidcastro@unicesar.edu.co>
2025-10-25 11:40:38 -0500

    feat: enhance NavBar component with improved dropdown menu layout and styling

--- src/components/organisms/NavBar.jsx
+++ src/components/organisms/NavBar.jsx
@@ -51,67 +51,77 @@ export default function NavBar() {
   }, [open]);
 
   return (
-    <nav
-      className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${
-        scrolled
-          ? "bg-transparent backdrop-blur-md shadow-lg"
-          : "bg-transparent"
-      }`}
-    >
-      <div className="px-[5%] py-2 lg:py-4 flex items-center justify-between">
-        <a href="/" className="text-2xl font-bold text-white cursor-pointer">
-          moov<span className="text-secondary">i</span>x
-        </a>
-        <ul className="hidden gap-8 font-medium lg:flex text-primary">
-          {links.map((link) => (
-            <li key={link.name} className="relative cursor-pointer group">
-              <a
-                href={link.href}
-                onClick={(e) => handleLinkClick(e, link.href)}
-              >
-                {link.name}
-              </a>
-            </li>
-          ))}
-        </ul>
-        <button
-          className={`lg:hidden  flex rounded-md px-2 py-2 gap-1 font-bold text-base items-center ${
-            open ? "bg-primary text-bg-secondary" : "text-primary "
-          }`}
-          onClick={() => setOpen(!open)}
-        >
-          Menú
-          {open ? (
-            <ChevronUp size={18} strokeWidth={3} />
-          ) : (
-            <ChevronDown size={18} strokeWidth={3} />
-          )}
-        </button>
-      </div>
+    <>
+      <nav
+        className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 ${
+          scrolled
+            ? "bg-transparent backdrop-blur-md shadow-lg"
+            : "bg-transparent"
+        }`}
+      >
+        <div className="px-[5%] py-2 lg:py-4 flex items-center justify-between">
+          <a href="/" className="text-2xl font-bold text-white cursor-pointer">
+            m<span className="text-secondary">oo</span>vix
+          </a>
+          <ul className="hidden gap-8 font-medium lg:flex text-primary">
+            {links.map((link) => (
+              <li key={link.name} className="relative cursor-pointer group">
+                <a
+                  href={link.href}
+                  onClick={(e) => handleLinkClick(e, link.href)}
+                >
+                  {link.name}
+                </a>
+              </li>
+            ))}
+          </ul>
+          <button
+            className={`lg:hidden  flex rounded-md px-2 py-2 gap-1 font-bold text-base lg:text-lg items-center ${
+              open ? "bg-primary text-bg-secondary" : "text-primary "
+            }`}
+            onClick={() => setOpen(!open)}
+          >
+            Menú
+            {open ? (
+              <ChevronUp size={18} strokeWidth={3} />
+            ) : (
+              <ChevronDown size={18} strokeWidth={3} />
+            )}
+          </button>
+        </div>
 
-      <div className="relative lg:hidden">
         {open && (
           <div className="relative lg:hidden">
-            {open && (
-              <div className="absolute right-4 top-0 p-2 bg-bg/80 backdrop:md text-primary rounded-xl py-3 w-56 z-40 backdrop-blur-md shadow-lg">
-                <ul className="flex flex-col text-sm font-medium">
-                  {links.map((link) => (
-                    <li key={link.name}>
-                      <a
-                        href={link.href}
-                        onClick={(e) => handleLinkClick(e, link.href)}
-                        className="block px-5 py-2 hover:bg-primary hover:text-bg rounded-lg transition-colors font-semibold"
-                      >
-                        {link.name}
-                      </a>
-                    </li>
-                  ))}
-                </ul>
-              </div>
-            )}
+            <div className="absolute right-[5%] top-2 p-2 bg-bg/80 text-primary rounded-xl py-3 w-auto z-90 backdrop-blur-md shadow-lg transition-colors duration-300 ">
+              <ul className="flex flex-col text-base md:text-lg font-bold">
+                {links.map((link, index) => (
+                  <li key={link.name} className="py-1">
+                    <a
+                      href={link.href}
+                      onClick={(e) => handleLinkClick(e, link.href)}
+                      className={`block px-5 py-2 hover:bg-primary hover:text-bg-secondary rounded-lg transition-colors ${
+                        index === links.length - 1
+                          ? "bg-secondary text-primary"
+                          : ""
+                      }`}
+                    >
+                      {link.name}
+                    </a>
+                  </li>
+                ))}
+              </ul>
+            </div>
           </div>
         )}
-      </div>
-    </nav>
+      </nav>
+
+      {/* Overlay con blur para toda la página */}
+      {open && (
+        <div
+          className="fixed inset-0 bg-transparent backdrop-blur-sm z-40 lg:hidden"
+          onClick={() => setOpen(false)}
+        />
+      )}
+    </>
   );
 }
